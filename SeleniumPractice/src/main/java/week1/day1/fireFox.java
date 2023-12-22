c73496c62a09c6c3dec1d2777659f81df7dd4087
narmatharamshankar@gmail.com <narmatharamshankar@gmail.com>
2023-12-22 13:40:07 +1100

    week1-day1

---
+++ SeleniumPractice/src/main/java/week1/day1/fireFox.java
@@ -0,0 +1,21 @@
+package week1.day1;
+
+public class fireFox {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float browserVersion = 100.2f ;
+		String browserName = "firefox" ;
+	    boolean isVisible = true ;
+		int releaseYear = 1998 ;
+		char browserLogo = 'f' ;
+		
+		System.out.println("browser Version is "+browserVersion);
+		System.out.println("Browser Name is "+browserName);
+		System.out.println("isVisible is " + isVisible);
+		System.out.println("releaseYear is "+releaseYear);
+		System.out.println("browserLogo is "+browserLogo);	
+
+	}
+
+}
